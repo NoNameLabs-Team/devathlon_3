b2a5f2328bc5558e9cd5bc8a2c91afdbe811cbc9
PhoenixofForce <phoenixofforce@gmail.com>
2016-07-16 19:43:06 +0200

    [+]Level-Mana-Converter: You can remove 1 level to gain 2 manapoints

---
+++ DevAthlon_3_1/src/de/nnl/devathlon_3/spellbook/LevelToMana.java
@@ -0,0 +1,56 @@
+package de.nnl.devathlon_3.spellbook;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.material.MaterialData;
+
+import de.nnl.devathlon_3.mana.ManaHandler;
+import de.nnl.devathlon_3.spells.Spell;
+
+public class LevelToMana implements Spell{
+
+	private ManaHandler mh;
+	public LevelToMana(ManaHandler mh){
+		this.mh = mh;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public MaterialData[] getIngredients() {
+		return new MaterialData[]{new MaterialData(Material.DIAMOND),new MaterialData(Material.DIAMOND),new MaterialData(Material.GOLDEN_APPLE),new MaterialData(Material.RAW_FISH, (byte) 3),new MaterialData(Material.GOLDEN_APPLE),new MaterialData(Material.EMERALD),new MaterialData(Material.EMERALD)};
+	}
+
+	@Override
+	public String getLore() {
+		return "Converts 1 Level to 2 Mana";
+	}
+
+	@Override
+	public String getName() {
+		return "Level-Mana-Converter";
+	}
+
+	@Override
+	public boolean isReusable() {
+		return false;
+	}
+
+	@Override
+	public int getManaCost() {
+		return 2;
+	}
+
+	@Override
+	public boolean onRightClick(Player p) {
+		if(p.getLevel() < 1){
+			return false;
+		}else{
+			
+			p.setLevel(p.getLevel() - 1);
+			mh.addMana(p, 2);
+			return true;
+			
+		}
+	}
+	
+}
