f0abd4248e972a1c0b9d8ce7a80e89990c727d2c
PhoenixofForce <phoenixofforce@gmail.com>
2016-07-16 18:40:44 +0200

    [/]Changed WaterSpell

--- DevAthlon_3_1/src/de/nnl/devathlon_3/spellbook/WaterSpell.java
+++ DevAthlon_3_1/src/de/nnl/devathlon_3/spellbook/WaterSpell.java
@@ -1,6 +1,7 @@
 package de.nnl.devathlon_3.spellbook;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.material.MaterialData;
@@ -39,14 +40,15 @@ public class WaterSpell implements Spell{
 
 	@Override
 	public void onRightClick(Player p) {
-		ProjectileUtil.fireProjectile(p, EntityType.SNOWBALL, 2.0, 0.0);
 		for(int x = p.getLocation().getBlockX() - 4; x < p.getLocation().getBlockX() + 4; x++){
-			for(int y = p.getLocation().getBlockY() - 4; y < p.getLocation().getBlockY() + 4; y++){
+			for(int y = p.getLocation().getBlockY() - 1; y < p.getLocation().getBlockY() + 1; y++){
 				for(int z = p.getLocation().getBlockZ() - 4; z < p.getLocation().getBlockZ() + 4; z++){
 					
-					if(p.getLocation().getWorld().getBlockAt(x, y, z).getType() == Material.FIRE){
-						p.getLocation().getWorld().getBlockAt(x, y, z).setType(Material.AIR);
-					}
+					Block b = p.getLocation().getWorld().getBlockAt(x, y, z);
+					
+					if(b.getType() == Material.FIRE)b.setType(Material.AIR);
+					else if(b.getType() == Material.WATER)b.setType(Material.ICE);
+					else if(b.getType() == Material.LAVA)b.setType(Material.OBSIDIAN);
 					
 				}
 			}
