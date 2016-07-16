20eef1489a58f6b2389eaee58cf055bde96ee5b9
PhoenixofForce <phoenixofforce@gmail.com>
2016-07-16 21:07:19 +0200

    [/]Wrong Location

--- DevAthlon_3_1/src/de/nnl/devathlon_3/spellbook/FelSpell.java
+++ DevAthlon_3_1/src/de/nnl/devathlon_3/spellbook/FelSpell.java
@@ -71,11 +71,16 @@ public class FelSpell implements Spell{
 						else if(b.getType() == Material.DIRT)b.setData((byte) 1);
 						else if(b.getType() == Material.SAPLING || b.getType() == Material.YELLOW_FLOWER|| b.getType() == Material.RED_ROSE|| b.getType() == Material.YELLOW_FLOWER)b.setType(Material.DEAD_BUSH);
 						else if(b.getType() == Material.DOUBLE_PLANT)b.setType(Material.REDSTONE);
-						else if(b.getType() == Material.LOG || b.getType() == Material.LOG_2)b.setType(Material.HUGE_MUSHROOM_1);
-						else if(b.getType() == Material.LEAVES || b.getType() == Material.WATER_LILY ||b.getType() == Material.VINE || b.getType() == Material.LEAVES_2)b.setType(Material.HUGE_MUSHROOM_2);
+						else if(b.getType() == Material.LOG || b.getType() == Material.LOG_2){
+							if(Util.RANDOM.nextInt(10) < 3)b.setType(Material.HUGE_MUSHROOM_1);
+							else b.setType(Material.AIR);
+						}
+						else if(b.getType() == Material.LEAVES || b.getType() == Material.WATER_LILY ||b.getType() == Material.VINE || b.getType() == Material.LEAVES_2)
+							if(Util.RANDOM.nextInt(10) < 3)b.setType(Material.HUGE_MUSHROOM_2);
+							else b.setType(Material.AIR);
 						else if(b.getType() == Material.STONE) b.setType(Material.COBBLESTONE);
 						else if(b.getType() == Material.LONG_GRASS){
-							switch(Util.RANDOM.nextInt(6)){
+							switch(Util.RANDOM.nextInt(2)){
 							case 0:
 								b.setType(Material.WEB);
 								break;
@@ -83,7 +88,6 @@ public class FelSpell implements Spell{
 								b.setType(Material.FIRE);
 								break;
 							}
-							b.setType(Material.AIR);
 						}
 						else if(b.getType() == Material.SAND || b.getType() == Material.GRAVEL) b.setType(Material.SOUL_SAND);
 						else if(b.getType() == Material.WATER)b.setType(Material.LAVA);
